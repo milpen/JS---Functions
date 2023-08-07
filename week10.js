d2cf39146f29c89379865c4ca5b6e3e1ba9f103d
milpen <milpen@mail.ru>
2023-08-07 21:14:08 +0500

    practicum

--- week10.js
+++ week10.js
@@ -1,123 +1,172 @@
 //Задание 1
 // Напишите функцию firstFunction, которая при нажатии кнопки выводит в консоль переменную first.
-let first = 8;
-
-function firstFunction() {}
-
-console.log(firstFunction());
+//  let first = 8; //объявлена глобальная переменная
+// function firstFunction(first) {  // создаем функцию и передаем ей параметр first
+//  return first;
+//  }
+//  console.log(firstFunction(first)); // в консоли вызываем функцию с аргументом
 
 //Задание 2
 // Напишите функцию sum, которая принимает два параметра a и b, складывает их и выводит в консоль результат.
-let a = 5;
+/*let a = 5;
 let b = 3;
 
-function sum(a, b) {}
+function sum(a, b) {
+  return (a + b);
+}
 
-console.log(sum(a, b));
+console.log(2, sum(a, b));*/
 
 //Задание 3
 // Напишите функцию multiply, которая принимает два параметра x и y, перемножает их и выводит в консоль результат.
-let x = 4;
+/*let x = 4;
 let y = 2;
 
-function multiply(x, y) {}
+function multiply(x, y) {
+  return (x * y);
+}
 
-console.log(multiply(x, y));
+console.log(3, multiply(x, y));*/
 
 //Задание 4
 // Напишите функцию greet, которая принимает один параметр name и выводит в консоль приветствие с этим именем.
-let firstName = 'Alice';
+/*let firstName = 'Alice';
 
-function greet(firstName) {}
+function greet(firstName) {
+  console.log(4, "Hello " + firstName);
+}
 
-greet(firstName);
+greet(firstName);*/
 
 //Задание 5
 // Напишите функцию calculateArea, которая принимает два параметра width и height, вычисляет площадь прямоугольника и выводит в консоль результат.
-let width = 6;
+/*let width = 6;
 let height = 4;
 
-function calculateArea(width, height) {}
+function calculateArea(width, height) {
+  return (width * height);
+}
 
-console.log(calculateArea(width, height));
+console.log(5, calculateArea(width, height));*/
 
 //Задание 6
 // Напишите функцию printMessage(), которая принимает один параметр message и выводит его в консоль. Если параметр не передан, используйте значение по умолчанию: "Сообщение отсутствует".
+// function printMessage(message="Сообщение отсутствует") {
+//   console.log(6, message);
+// }
+
+// printMessage("hi");// параметр передан
+// printMessage();// параметр не передан
 
 //Задание 7
-// Напишите функцию secondFunction, которая принимает два параметра: c и d, и выводит в консоль их сумму. Если значения параметров не переданы, используйте значения по умолчанию: c = 5 и d = 10.
-let c = 5;
-let d = 10;
+// // Напишите функцию secondFunction, которая принимает два параметра: c и d, и выводит в консоль их сумму. Если значения параметров не переданы, используйте значения по умолчанию: c = 5 и d = 10.
+// let c = 5;
+// let d = 10;
 
-function secondFunction() {}
+// function secondFunction(c=5, d=10) {
+//   return c + d;
+// }
 
-console.log(secondFunction()); //Выведет 15
-//Обратите ваше внимание, что мы можем передать в функцию любые аргументы и secondFunction подставит их на место параметров соответственно, перемножит их и вернёт результат.
-console.log(secondFunction(2, 3)); //Выведет 5
+// console.log(secondFunction()); //Выведет 15
+// //Обратите ваше внимание, что мы можем передать в функцию любые аргументы и secondFunction подставит их на место параметров соответственно, перемножит их и вернёт результат.
+// console.log(secondFunction(2, 3)); //Выведет 5
 
 //Задание 8
 // Напишите функцию thirdFunction, которая принимает два параметра: f и g, и выводит в консоль их произведение. Если только одно значение передано, используйте его как оба параметра функции. Если ни одно значение не передано, используйте значения по умолчанию: f = 2 и g = 3.
-let f = 2;
-let g = 3;
+// let f = 2;
+// let g = 3;
 
-function thirdFunction(f = 2, g = 3) {}
+// function thirdFunction(f = 2, g = 3) {
+//   return f * g;
+// }
 
-console.log(thirdFunction()); //Выведет 6
-//Обратите ваше внимание, что мы можем передать в функцию любые аргументы и thirdFunction подставит их на место параметров соответственно (если пармаметр не задан, то он подставится по умлочанию), перемножит их и вернёт результат.
-console.log(thirdFunction(4)); //Выведет 12
-console.log(thirdFunction(5, 6)); //Выведет 30
+// console.log(8, thirdFunction()); //Выведет 6
+// //Обратите ваше внимание, что мы можем передать в функцию любые аргументы и thirdFunction подставит их на место параметров соответственно (если пармаметр не задан, то он подставится по умлочанию), перемножит их и вернёт результат.
+// console.log(9, thirdFunction(4)); //Выведет 12
+// console.log(10, thirdFunction(5, 6)); //Выведет 30
 
 //Задание 9
 // Напишите функцию приветствия (название придумайте самостоятельно), которая принимает один параметр guestName и возвращает приветствие в формате "Привет, {guestName}!". Если параметр не передан, используйте значение по умолчанию: "Гость". Чтобы увидеть результат в консоли, вызовите функцию внутри console.log.
 
+//  function guestGreeting (guestName = "Гость") {
+//    return "Привет, " + guestName +"!";
+//  }
+// console.log(11, guestGreeting("Anna"));
+// console.log(12, guestGreeting());
 //Задание 10
 // Создайте функцию calculateDifference с помощью Function Expression и правильно используйте её, чтобы вычесть два числа и вывести результат в консоль.
-// Ваш код
+ //Ваш код
 
-const result = calculateDifference(5, 3);
+// const result = function calculateDifference(a, b) {
+//   a = 5;
+//   b = 3;
+//   return a - b;
+// };
 
-console.log(result); //Выведет 2
+// console.log(13, result()); //Выведет 2
 
 //Задание 11
 // Напишите функцию calculateProduct с помощью Function Expression, которая будет принимать два параметра. Функция должна перемножить значения и вывести результат в консоль.
 
-const total = calculateProduct(5, 3);
+// const total = function calculateProduct(e, f) {
+//   return e * f;
+// };
 
-console.log(total); //Выведет 15
+// console.log(14, total(2, 3)); //Выведет 15
 
 //Задание 12
 // Напишите стрелочную функцию calculateSquare, которая будет принимать один параметр num. Внутри функции объявите локальную переменную result и присвойте ей значение, равное квадрату числа num. Затем верните значение переменной result из функции.
+// const squaredNumber = num => {
+//   const result = num * num;
+//   return result;
+// };
 
-const squaredNumber = calculateSquare(5);
-
-console.log(squaredNumber); // Выведет 25
+// console.log(15, squaredNumber(5)); // Выведет 25
 
 //Задание 13
 // Напишите  стрелочную функцию sayThree, которая будет выводить в консоль сообщение "Три!"
-
+// const sayThree = () => console.log(16, "Три!");
+// sayThree();
 //Задание 14
 // Напишите функцию findAnimal, которая будет выводить в консоль значения переменных cat и rabbit. Переменная cat объявлена внутри функции, а переменная rabbit объявлена во внешней области видимости.
+// const rabbit = "Кролик";
+// function findAnimal () {
+//   const cat = "Кот";
+//   console.log(17, cat, rabbit);
+// }
+// findAnimal();
 
 //Задание 15
 // Допишите программу, которая вычисляет сколько лет прошло с летней олимпиады в Токио, Япония. Подсказка: 1. нужно создать переменную с текущим годом. 2. при вызове функции в круглые скобки нужно передать два параметра: текущий год и год летней олимпиады в Токио.
-const lastOlympicsYear = 2021;
+// const lastOlympicsYear = 2021;
+// const currentYear = 2023;
 
-function calculateYearsSinceLastOlympics(a, b) {
-	return a - b;
-}
-
-console.log(
-	'С момента летней олимпиады в Токио прошло ' + calculateYearsSinceLastOlympics() + ' года',
-);
+// function calculateYearsSinceLastOlympics(a, b) {
+// 	return a - b;
+// }
+// console.log(19, 
+// 	'С момента летней олимпиады в Токио прошло ' + calculateYearsSinceLastOlympics(currentYear, lastOlympicsYear) + ' года',
+// );
 
 //Задание 16
 // Напишите программу, которая вычисляет возраст пользователя на основе его года рождения, будет выводить результат в консоль.
-const friendYearOfBirth = 1985;
-
-function calculateAge() {}
+// const friendYearOfBirth = 1985;
+// const currentYear = 2023;
+// function calculateAge(a, b) {
+//   return a - b;
+// };
+// console.log(20, "Ваш возраст: " + calculateAge(currentYear, friendYearOfBirth) + " лет");
 
 //Задание 17
 // Напишите функцию которая принимает ваш год рождения и возвращает ваш возраст. Обратите ваше внимание, что в глобальной зоне видимости есть переменная с текущим годом.
-
+// const currentYear = 2023;
+// function myAge (birthYear) {
+//   return (currentYear - birthYear);
+// }
+// console.log("Ваш возраст составляет: " + myAge(1982) + " год");
 //Задание 18
 // Напишите функцию, которая должна возводить число в заданную степень. Результат должен выводить в консоль в формате: console.log("Результат: " + result);
+// function getNumberDegree (a, b) {
+//   return a**b;
+// };
+// console.log (22, "Результат: " + getNumberDegree(3, 5));
